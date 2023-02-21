7fd48b56cf4e11b7e7157cec451db0597ec64bb2
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-02-21 20:51:35 +0500

    add tooltip in header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,25 @@
-import React from "react";
-import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
-import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import React from 'react';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import { IconButton, Tooltip } from '@mui/material';
 
 const Header = ({ children }) => {
-  return ( 
-    <div className="w-full flex justify-between h-[60px] items-center pt-3 p-2 bg_color">
-      <h1 className="w-[100px] font-bold text-[15px] ">{children}</h1>
-      <div className="flex w-[250px] h-full justify-between items-center">
-        <NotificationsActiveOutlinedIcon />
-        <NotificationsNoneOutlinedIcon />
-        <p className="text-[14px] ">Smart Client Office</p>
+  return (
+    <div className='w-full flex justify-between h-[60px] items-center pt-3 p-2 bg_color'>
+      <h1 className='w-[100px] font-bold text-[15px] '>{children}</h1>
+      <div className='flex w-[250px] h-full justify-between items-center'>
+        <Tooltip title='Уведомления'>
+          <IconButton style={{ color: 'white' }}>
+            <NotificationsActiveOutlinedIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title='Обновление'>
+          <IconButton style={{ color: 'white' }}>
+            <NotificationsNoneOutlinedIcon />
+          </IconButton>
+        </Tooltip>
+        <p className='text-[14px] '>Smart Client Office</p>
         <AccountCircleOutlinedIcon />
       </div>
     </div>
